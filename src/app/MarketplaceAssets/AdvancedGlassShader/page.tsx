d315ece94fa536f0c3a3b86ecbdbaacd0f80aa25
Antidamage <adeline@skull.co.nz>
2024-10-08 15:25:05 +1300

    Fixing lint errors

--- src/app/MarketplaceAssets/AdvancedGlassShader/page.tsx
+++ src/app/MarketplaceAssets/AdvancedGlassShader/page.tsx
@@ -1,6 +1,6 @@
 export default function AdvancedGlassShader({ }: {}) {
     return (<>
         <p className="ps-10 leading-none tracking-tight font-bold text-2x1 text-nowrap">Coming soon.</p>
-        <h1 className="mb-4 font-extrabold leading-none tracking-tight text-4xl text-nowrap ps-10 pe-2">Advamced Glass Shader</h1>
+        <h1 className="mb-4 font-extrabold leading-none tracking-tight text-4xl text-nowrap ps-10 pe-2">Advanced Glass Shader</h1>
     </>);
 }

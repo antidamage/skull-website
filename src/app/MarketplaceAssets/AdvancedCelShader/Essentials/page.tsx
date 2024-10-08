61547d88c9ddfcfbd1409bdeed364b89792fb863
Antidamage <adeline@skull.co.nz>
2024-10-08 15:22:57 +1300

    Fixing lint errors

---
+++ src/app/MarketplaceAssets/AdvancedCelShader/Essentials/page.tsx
@@ -0,0 +1,6 @@
+export default function Essentials({ }: {}) {
+    return (<>
+        <p className="ps-10 leading-none tracking-tight font-bold text-2x1 text-nowrap">Coming soon.</p>
+        <h1 className="mb-4 font-extrabold leading-none tracking-tight text-4xl text-nowrap ps-10 pe-2">Advamced Cel Shader Essentials</h1>
+    </>);
+}

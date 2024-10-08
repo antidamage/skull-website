83a10c67bc49ee7561c5fa987b514ee5b85ee1e9
Antidamage <adeline@skull.co.nz>
2024-10-08 15:26:22 +1300

    Fixing lint errors

--- src/app/MarketplaceAssets/AdvancedCelShader/page.tsx
+++ src/app/MarketplaceAssets/AdvancedCelShader/page.tsx
@@ -1,4 +1,4 @@
-export default function Advanced Cel Shader({}: {}) {
+export default function AdvancedCelShader({ }: {}) {
     return (<>
         <p className="ps-10 leading-none tracking-tight font-bold text-2x1 text-nowrap">Coming soon.</p>
         <h1 className="mb-4 font-extrabold leading-none tracking-tight text-4xl text-nowrap ps-10 pe-2">Advanced Cel Shader Series</h1>

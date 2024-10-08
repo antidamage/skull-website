1b925b4f01f72b8ef2358aa908668ea1a3f77579
Antidamage <adeline@skull.co.nz>
2024-10-08 16:57:57 +1300

    Launch links in a new tab

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -11,7 +11,7 @@ export default function Contact({ }: {}) {
                 <div className="flex inline mt-5">
                     <div className="ps-10 leading-none tracking-tight font-bold text-lg text-nowrap">
                         <div>
-                            <a href="https://discord.gg/RAEJEhW" className="flex flex-nowrap align-baseline text-lg">
+                            <a href="https://discord.gg/RAEJEhW" className="flex flex-nowrap align-baseline text-lg" target="_blank">
                                 Discord
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 flex flex-nowrap ml-2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
--- src/app/HireAddie/page.tsx
+++ src/app/HireAddie/page.tsx
@@ -21,15 +21,15 @@ export default function HireAddie({ }: {}) {
             <br /><br />
             Addie is also a talented web developer who regularly runs the gamut of dev-ops, server admin, database admin, frontend and backend.
             <br /><br />
-            <a className="flex flex-nowrap align-baseline text-lg font-bold" href="https://docs.google.com/document/d/1TZotQfqw4h_oAkPLhlSFk2TJ6eQkrNEJ1GGCGape8cA/edit?usp=sharing">
+            <a className="flex flex-nowrap align-baseline text-lg font-bold" href="https://docs.google.com/document/d/1TZotQfqw4h_oAkPLhlSFk2TJ6eQkrNEJ1GGCGape8cA/edit?usp=sharing" target="_blank">
                 Adeline Cook - Curriculum Vitae
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 flex flex-nowrap ml-2 text-nowrap">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25" />
                 </svg>
             </a>
             <br />
-            <a className="flex flex-nowrap align-baseline text-base font-bold" href="https://github.com/antidamage/skull-website">
-                This website uses Next.js and Tailwind. View it on Github
+            <a className="flex flex-nowrap align-baseline text-base font-bold" href="https://github.com/antidamage/skull-website" target="_blank">
+                This website uses Next.js and Tailwind - Github
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 flex flex-nowrap ml-2 text-nowrap">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
                 </svg>

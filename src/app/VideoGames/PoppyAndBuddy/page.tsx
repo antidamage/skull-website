489c07a025b1920d81c19471583eb55c4276ffbe
Antidamage <adeline@skull.co.nz>
2024-10-08 15:30:59 +1300

    Fixing lint errors

---
+++ src/app/VideoGames/PoppyAndBuddy/page.tsx
@@ -0,0 +1,6 @@
+export default function PoppyAndBuddy({ }: {}) {
+    return (<>
+        <p className="ps-10 leading-none tracking-tight font-bold text-2x1 text-nowrap">Coming soon.</p>
+        <h1 className="mb-4 font-extrabold leading-none tracking-tight text-4xl text-nowrap ps-10 pe-2">Poppy and Buddy</h1>
+    </>);
+}
